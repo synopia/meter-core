ccdd10fce115b32d7bd40c44e69576c6a5ac935d
synopia <paul.fritsche@gmail.com>
2024-03-26 19:18:45 +0100

    added SkillCooldownNotify

--- src/packets/generated/definitions/PKTSkillCooldownNotify.ts
+++ src/packets/generated/definitions/PKTSkillCooldownNotify.ts
@@ -7,12 +7,11 @@ export type PKTSkillCooldownNotify = {
 export function read(buf: Buffer): PKTSkillCooldownNotify {
     const reader = new Read(buf);
     const data = {} as PKTSkillCooldownNotify;
-    reader.skip(6);
-    data.cooldown2 = reader.f32();
-    reader.skip(1)
+    reader.skip(4);
     data.cooldown1 = reader.f32();
+    data.cooldown2 = reader.f32();
     data.skillId = reader.u32();
     return data;
 }
 export const name= "PKTSkillCooldownNotify";
-export const opcode = 43413;
+export const opcode = 49035;
